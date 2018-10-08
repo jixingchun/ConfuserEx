878f9b2ca7f22c397e6b8c332156fcab27a2f6f3
jxc <jixingchun@hotmail.com>
2018-10-08 13:27:52 +0800

    添加去除水印处理

---
+++ Test.PatternFunction2/ConsoleApp2/readme.md
@@ -0,0 +1,8 @@
+﻿# Markdown file
+
+plugin：
+可以放置第三方混淆插件
+
+D:\study\github_online\ConfuserEx\plugin\Confuser.Protections.HoLLy\Confuser.Protections.HoLLy.dll
+
+作用是除去 confuser属性标识
---
+++ Test.PatternFunction3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using DIAView.Common.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemLog.Log.Info("xxxxxxxx");
+            Console.WriteLine("press enter key to exist");
+            Console.ReadLine();
+        }
+    }
+}
